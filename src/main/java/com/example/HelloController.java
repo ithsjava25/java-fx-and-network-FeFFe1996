8b66e889d17c76cf13d60bb54846b70d1b68d1fa
Felix Eriksson <felix.eriksson1996@hotmail.com>
2025-10-28 12:38:07 +0100

    textFlow

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.text.TextFlow;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -12,7 +13,7 @@ import javafx.scene.control.ListView;
 public class HelloController {
     public ListView<String> listView;
     private final HelloModel model = new HelloModel();
-    public Label messageBoard;
+    public TextFlow messageBoard;
 
     @FXML
     private Label messageLabel;
