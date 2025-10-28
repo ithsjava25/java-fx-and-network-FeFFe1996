b4b0e2345470d5a64981e70ec147cd5959ee6e43
Felix Eriksson <felix.eriksson1996@hotmail.com>
2025-10-28 08:48:28 +0100

    messageboard property

--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -7,10 +7,19 @@ import javafx.beans.property.StringProperty;
  * Model layer: encapsulates application data and business logic.
  */
 public class HelloModel {
-    /**
-     * Returns a greeting based on the current Java and JavaFX versions.
-     */
-    public StringProperty messageBoard = new SimpleStringProperty();
+    private final StringProperty messageBoardProperty;
+    public HelloModel() {
+        messageBoardProperty = new SimpleStringProperty("");
+    }
+
+    public void setMessageBoardProperty(String messageBoardProperty) {
+        messageBoardProperty.isEmpty();
+    }
+
+    public String getMessageBoard() {
+        return messageBoardProperty.get();
+    }
+
     public String getGreeting() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
